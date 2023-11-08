5eaf4eebd8df5f0134bf1be775b629bd01832166
davenport1 <61665447+davenport1@users.noreply.github.com>
2023-11-07 16:32:45 -0800

    blog page skeleton finished

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,10 +1,22 @@
+import NavBar from "@/components/NavBar";
 import React from "react";
+
 
 export default function Page() {
     return (
         <div>
             <title>The Gnar Blog</title>
-            <h1>Blog</h1>
+            <main className="min-h-screen bg-[url('../../public/backcountryascent1.jpg')] bg-cover bg-top">
+            <NavBar />
+            <nav className="py-10 mb-12 flex justify-between text-white">
+            </nav>
+            <section className=" bg-slate-700 text-white py-4 sm:py-8 md:py-16 shadow-xl rounded-xl sm:m-2 md:m-8 transition duration-300 transform hover:scale-105">
+            <div className="container mx-auto text-center">
+            <h2 className="text-3xl font-bold mb-4 md:mb-16 lg:mb-32">Full Send Waxing Community Shredder Posts</h2>
+            <p>Coming soon....</p>
+            </div>
+            </ section>
+            </main>
         </div>
     )
 }

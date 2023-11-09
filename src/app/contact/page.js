748ce713366574654feaf02531a73eda2a35d4eb
davenport1 <61665447+davenport1@users.noreply.github.com>
2023-11-09 07:06:28 -0800

    fixed footer

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -12,7 +12,7 @@ export default function ContactPage() {
             <title>FSW Contact</title>
             <NavBar className="z-10 relative" />
             <main className="min-h-screen bg-[url('../../public/threeboarders.jpg')] bg-cover bg-top">
-            <nav className="py-10 mb-12 flex justify-between text-white">
+            <nav className="py-10 flex justify-between text-white">
                 </nav>
                 <div className="flex justify-center items-center h-full mb-8">
                     <Contact />

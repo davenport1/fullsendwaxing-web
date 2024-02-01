753ea4f09e0517a6bacd81f9aac467f49fd9c7ef
Matthew <davenmport@gmail.com>
2024-01-31 21:40:29 -0800

    client calls

--- src/api/UsersApi.js
+++ src/api/UsersApi.js
@@ -1,20 +1,37 @@
-import axios from "axios";
 import httpClient from "@/api/Client";
 
-export const createUser = async (email, password) => {
+const createUserEnpoint = '/createuser';
+const loginEndpoint = '/login';
+const logoutEndpoint = '/logout';
+const changePasswordEndpoint = '/notimplemented';
 
+export const createUser = async (email, password) => {
+    httpClient.post(createUserEnpoint)
+        .then(function (response) {
+            // retrieve token to use for this user from the response.
+            // do we login when creating user?
+        })
 }
 
 export const login = async(email, password) => {
-
+    httpClient.post(loginEndpoint)
+        .then(function (response) {
+            // retrieve token to use for this user from the response.
+        })
 }
 
 export const logout = async (token) => {
-
+    httpClient.post(logoutEndpoint)
+        .then(function (response) {
+            // should just handle 200 ok
+        })
 }
 
 export const changePassword = async (email, newPassword) => {
-
+    httpClient.post(changePasswordEndpoint)
+        .then(function (response) {
+            // login as well and retrieve token?
+        })
 }
 
 module.exports = {
